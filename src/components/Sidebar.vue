011af965a0f4e48b520dd528073113b3536acc70
Yahya Bakleh <123378314+yahyabakleh12@users.noreply.github.com>
2025-07-17 09:08:55 +0400

    Integrate Bootstrap layout

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -16,14 +16,18 @@ function logout() {
 </script>
 
 <template>
-  <aside class="sidebar">
-    <h2>On Street Parking</h2>
-    <ul>
-      <li class="title">On Street Parking</li>
-      <li :class="{ active: route.path === '/tickets' }" @click="goTickets">
+  <aside class="sidebar bg-dark text-white p-3 h-100">
+    <h2 class="h5">On Street Parking</h2>
+    <ul class="list-unstyled mt-3">
+      <li class="title fw-bold mb-2">On Street Parking</li>
+      <li
+        class="mb-1 px-2 py-1 rounded"
+        :class="{ 'bg-secondary': route.path === '/tickets' }"
+        @click="goTickets"
+      >
         Tickets
       </li>
-      <li class="logout" @click="logout">Logout</li>
+      <li class="logout mt-3" @click="logout">Logout</li>
     </ul>
   </aside>
 </template>
--- src/views/login.vue
+++ src/views/login.vue
@@ -20,11 +20,29 @@ async function doLogin() {
 </script>
 
 <template>
-  <div class="login">
-    <h2>Login</h2>
-    <input v-model="username" placeholder="Username" />
-    <input v-model="password" type="password" placeholder="Password" />
-    <button @click="doLogin">Login</button>
-    <p v-if="error">{{ error }}</p>
+  <div class="container py-5">
+    <div class="row justify-content-center">
+      <div class="col-md-4">
+        <h2 class="mb-3 text-center">Login</h2>
+        <div class="mb-3">
+          <input
+            v-model="username"
+            type="text"
+            class="form-control"
+            placeholder="Username"
+          />
+        </div>
+        <div class="mb-3">
+          <input
+            v-model="password"
+            type="password"
+            class="form-control"
+            placeholder="Password"
+          />
+        </div>
+        <button class="btn btn-primary w-100" @click="doLogin">Login</button>
+        <p class="text-danger mt-2" v-if="error">{{ error }}</p>
+      </div>
+    </div>
   </div>
 </template>
