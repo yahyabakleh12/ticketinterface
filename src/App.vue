ab70a13c1483308d2a226762c37c77eee2077925
Yahya Bakleh <123378314+yahyabakleh12@users.noreply.github.com>
2025-07-17 09:45:23 +0400

    Fix App layout and adjust lint script

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import NavBar from '@/components/NavBar.vue'
-import Sidebar from '@/components/Sidebar.vue'
 </script>
 
 <template>
-  <div class="container" v-if="ticket">
-    <div class="row">
-      <div class="col-md-2 p-0 ms-auto">
-        <Sidebar />
-      </div>
-      <div class="col-md-10 ms-auto">
-        <NavBar title="Ticket Details" :notifications="1" />
-        <RouterView />
-      </div>
-    </div>
-  </div>
+  <RouterView />
 </template>
 
 <style scoped></style>
