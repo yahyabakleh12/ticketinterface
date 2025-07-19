b1c7b2420ac8bd954e18f208ecaaed60e7c84bf1
yahyabakleh12 <yahya@parkonic.com>
2025-07-19 14:25:17 +0400

    changing server configration

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:18001',
+  baseURL: 'http://10.11.5.103:18001',
 })
 
 export function login(username, password) {
--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
